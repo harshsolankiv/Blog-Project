347d36f2835179a3390e40c987a1cc851c913099
harshsolankiv <harshbhai.solanki_19@sakec.ac.in>
2022-02-24 13:41:47 +0530

    atharva

--- Blog_Post/blog/views.py
+++ Blog_Post/blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render, HttpResponse
-
+from matplotlib.style import context
+from blog.models import Post
 # Create your views here.
 
 
 def blogHome(request):
-    return render(request, "blog/blogHome.html")
+    allPosts = Post.objects.all()
+    context={
+        'allPosts':allPosts,
+    }
+    return render(request, "blog/blogHome.html",context)
 
 
 def blogPost(request, slug):
-    return render(request, "blog/blogPost.html")
+    post = Post.objects.filter(slug=slug)[0]
+    context={
+        'post':post,
+    }
+    return render(request, "blog/blogPost.html",context)
--- Blog_Post/home/views.py
+++ Blog_Post/home/views.py
@@ -1,6 +1,9 @@
 import email
 from django.shortcuts import render, HttpResponse
+from matplotlib.pyplot import title
 from .models import Contact
+from blog.models import Post
+from django.contrib import messages
 # Create your views here.
 
 
@@ -13,7 +16,7 @@ def about(request):
 
 
 def contact(request):
-    from django.contrib import messages
+
     if request.method == "POST":
         name = request.POST['name']
         email = request.POST['email']
@@ -28,3 +31,21 @@ def contact(request):
             messages.success(
                 request, "Your Query has been sent successfuly!!!")
     return render(request, "home/contact.html")
+
+
+def search(request):
+    query = request.GET['query']
+    if len(query)>78:
+        allPosts = Post.objects.none()
+    else:
+        allPoststitle = Post.objects.filter(title__icontains=query)
+        allPostContent = Post.objects.filter(content__icontains=query)
+        allPosts = allPoststitle.union(allPostContent)
+    
+    if allPosts.count() ==0:
+        messages.warning(request, "No search results found please refine your query")
+        
+    params = {'allPosts':allPosts,
+              'query':query,
+              }
+    return render(request,'home/search.html',params)
