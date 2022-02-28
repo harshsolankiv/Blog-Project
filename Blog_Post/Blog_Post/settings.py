b298a8310c4eea1a4ff161829615d5d338f007a0
vishalrai20 <vishalbrai02@gmail.com>
2022-02-28 16:17:23 +0530

    till 97 done

--- Blog_Post/Blog_Post/settings.py
+++ Blog_Post/Blog_Post/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'blog.apps.BlogConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
--- Blog_Post/blog/views.py
+++ Blog_Post/blog/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.shortcuts import redirect, render, HttpResponse
 from matplotlib.style import context
 from blog.models import Post, BlogComment
+from blog.templatetags import extras
 # Create your views here.
 
 
@@ -19,10 +20,19 @@ def blogHome(request):
 
 def blogPost(request, slug):
     post = Post.objects.filter(slug=slug)[0]
-    comments = BlogComment.objects.filter(post=post)
+    comments = BlogComment.objects.filter(post=post, parent = None)
+    replies = BlogComment.objects.filter(post=post).exclude(parent = None)
+    replyDict = {}
+    for reply in replies:
+        if reply.parent.sno not in replyDict.keys():
+            replyDict[reply.parent.sno] = [reply]
+        else:
+            replyDict[reply.parent.sno].append(reply)
     context = {
         'post': post,
         'comments': comments,
+        'user' : request.user,
+        'replyDict' : replyDict,
     }
     return render(request, "blog/blogPost.html", context)
 
@@ -33,8 +43,15 @@ def postComment(request):
         user = request.user
         postSno = request.POST.get("postSno")
         post = Post.objects.get(sno=postSno)
-        comment = BlogComment(comment=comment, user=user, post=post)
+        parentSno = request.POST.get("parentSno")
+        if parentSno == "":
+            comment = BlogComment(comment=comment, user=user, post=post)
+            comment.save()
+            messages.success(request, "Your comment posted 👏👏")
+        else:
+            parent = BlogComment.objects.get(sno = parentSno)
+            comment = BlogComment(comment=comment, user=user, post=post, parent = parent)
         comment.save()
-        messages.success(request, "Your comment posted 👏👏")
+        messages.success(request, "Your reply posted 👏👏")
 
     return redirect(f"/blog/{post.slug}")
